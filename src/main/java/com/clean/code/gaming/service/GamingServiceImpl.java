e0ceeb686dc790326889c79e69131beefb8de088
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-11-29 19:40:31 +0530

    Clean Code - REFACTOR (Added Validation for checking if the player inputs are non-null.

--- src/main/java/com/clean/code/gaming/service/GamingServiceImpl.java
+++ src/main/java/com/clean/code/gaming/service/GamingServiceImpl.java
@@ -18,9 +18,25 @@ public class GamingServiceImpl{
 	 * @param GamingEnum
 	 * @return String
 	 */
-	public String determineGameWinner(GamingEnum player1Input, GamingEnum player2Input) {
-		GamingResponseEnum gamingResponse = player1Input.compare(player2Input);
-		String gamingResponseMessage = gamingResponse != null ? gamingResponse.getMessage() : null;
-		return gamingResponseMessage;
+	public String determineGameWinner(GamingEnum player1Input, GamingEnum player2Input) {		 
+		boolean arePlayerInputsValid = validatePlayerInputs(player1Input, player2Input);
+		if(arePlayerInputsValid){
+			GamingResponseEnum gamingResponse = player1Input.compare(player2Input);
+			String gamingResponseMessage =gamingResponse != null ? gamingResponse.getMessage() : null;
+			return gamingResponseMessage;
+		}
+		return null;
+	}
+
+	
+	/**
+	 * Validate if either of player inputs is not provided.
+	 * 
+	 * @param GamingEnum
+	 * @param GamingEnum
+	 * @return boolean - true if both inputs are non-null. Else returns false.
+	 */
+	private boolean validatePlayerInputs(GamingEnum player1Input, GamingEnum player2Input) {
+		return player1Input != null && player1Input != null;
 	}
 }
