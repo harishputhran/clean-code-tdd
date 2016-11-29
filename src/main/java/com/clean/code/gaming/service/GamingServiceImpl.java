57854e5d76d3dbefdf925136bc603cadf970fa09
Puthran, Harish <Harish.Puthran@in.tesco.com>
2016-11-29 03:29:30 +0000

    Clean Code Training
    CHANGES:
    1. Added the implementation for Rock Wins Scenario

--- src/main/java/com/clean/code/gaming/service/GamingServiceImpl.java
+++ src/main/java/com/clean/code/gaming/service/GamingServiceImpl.java
@@ -7,6 +7,9 @@ import com.clean.code.gaming.api.GamingService;
 public class GamingServiceImpl implements GamingService {
 	
 	private static final String GAME_TIED = "Game Tied.";
+	private static final String ROCK_WINS = "Rock crushes Scissors. Rock wins.";
+	private static final String ROCK = "Rock";
+	private static final String SCISSORS = "Scissors";
 
 	@Override
 	public String determineGameWinner(String player1Input, String player2Input) {
@@ -15,6 +18,9 @@ public class GamingServiceImpl implements GamingService {
 			return null;
 		}else if(StringUtils.equalsIgnoreCase(player1Input, player2Input)){
 			return GAME_TIED;
+		}else if((ROCK.equalsIgnoreCase(player1Input) && SCISSORS.equalsIgnoreCase(player2Input))
+					|| (ROCK.equalsIgnoreCase(player2Input) && SCISSORS.equalsIgnoreCase(player1Input))){
+			return ROCK_WINS;
 		}
 		return null;
 	}
