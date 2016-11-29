3d160e0ec8152d294f5f4d0ee65ee9f56479af0b
Puthran, Harish <Harish.Puthran@in.tesco.com>
2016-11-29 03:10:59 +0000

    Clean Code Training
    CHANGES:
    1. Added the implementation for Game Tied Scenario

--- src/main/java/com/clean/code/gaming/service/GamingServiceImpl.java
+++ src/main/java/com/clean/code/gaming/service/GamingServiceImpl.java
@@ -1,12 +1,18 @@
 package com.clean.code.gaming.service;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.clean.code.gaming.api.GamingService;
 
 public class GamingServiceImpl implements GamingService {
+	
+	private static final String GAME_TIED = "Game Tied.";
 
 	@Override
 	public String determineGameWinner(String player1Input, String player2Input) {
-		// TODO Auto-generated method stub
+		if(StringUtils.equalsIgnoreCase(player1Input, player2Input)){
+			return GAME_TIED;
+		}
 		return null;
 	}
 
