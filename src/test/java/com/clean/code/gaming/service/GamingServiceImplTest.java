e04dcc8f80a13964eda86a3a49a178e1a5d52f6a
Puthran, Harish <Harish.Puthran@in.tesco.com>
2016-11-29 03:26:09 +0000

    Clean Code Training
    CHANGES:
    1. Added Test Methods for "Rock Wins" Scenario - 
    Player(1/2) chooses Rock & Player(2/1) chooses Scissors.

--- src/test/java/com/clean/code/gaming/service/GamingServiceImplTest.java
+++ src/test/java/com/clean/code/gaming/service/GamingServiceImplTest.java
@@ -11,7 +11,9 @@ public class GamingServiceImplTest {
 	
 	private GamingServiceImpl classToTest = new GamingServiceImpl();
 	private static final String GAME_TIED = "Game Tied.";
+	private static final String ROCK_WINS = "Rock crushes Scissors. Rock wins.";
 	private static final String ROCK = "Rock";
+	private static final String SCISSORS = "Scissors";
 	
 	@Test
 	public void returnsNullFromDetermineGameWinnerWhenEitherOfPlayerInputsIsInvalid(){
@@ -22,6 +24,16 @@ public class GamingServiceImplTest {
 	@Test
 	public void returnsGameTiedFromDetermineGameWinnerWhenBothPlayersChooseSameObject(){
 		assertEquals(GAME_TIED, classToTest.determineGameWinner(ROCK, ROCK));
+	}
+	
+	@Test
+	public void returnsRockIsWinnerFromDetermineGameWinnerWhenPlayer1ChoosesRockAndPlayer2ChoosesScissors(){
+		assertEquals(ROCK_WINS, classToTest.determineGameWinner(SCISSORS, ROCK));
+	}
+	
+	@Test
+	public void returnsRockIsWinnerFromDetermineGameWinnerWhenPlayer2ChoosesRockAndPlayer1ChoosesScissors(){
+		assertEquals(ROCK_WINS, classToTest.determineGameWinner(ROCK, SCISSORS));
 	}
 
 }
