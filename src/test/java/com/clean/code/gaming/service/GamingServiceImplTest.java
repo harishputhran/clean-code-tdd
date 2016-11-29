4f089fa02727338c0b44cc20ab3e8ab7dc508cb6
Puthran, Harish <Harish.Puthran@in.tesco.com>
2016-11-29 03:07:21 +0000

    Clean Code Training
    CHANGES:
    1. Added TestCase for Game Tied scenario when both players choose same
    object.

--- src/test/java/com/clean/code/gaming/service/GamingServiceImplTest.java
+++ src/test/java/com/clean/code/gaming/service/GamingServiceImplTest.java
@@ -10,11 +10,18 @@ import org.junit.runners.JUnit4;
 public class GamingServiceImplTest {
 	
 	private GamingServiceImpl classToTest = new GamingServiceImpl();
+	private static final String GAME_TIED = "Game Tied.";
+	private static final String ROCK = "Rock";
 	
 	@Test
 	public void returnsNullFromDetermineGameWinnerAsTheImplementationIsNotDone(){
 		
 		assertNull(classToTest.determineGameWinner(null, null));
+	}
+	
+	@Test
+	public void returnsGameTiedFromDetermineGameWinnerWhenBothPlayersChooseSameObject(){
+		assertEquals(GAME_TIED, classToTest.determineGameWinner(ROCK, ROCK));
 	}
 
 }
